37bea5fb5fc7dba8c051d62f88c7e61cd2724f74
Korny Sietsma <korny@sietsma.com>
2020-09-17 20:38:17 +0100

    Added individual metrics pages and other content from blog post

--- src/components/citation_to_id.js
+++ src/components/citation_to_id.js
@@ -1,7 +1,4 @@
 export default function citation_to_id(citation) {
-    console.warn("citation_to_id", citation)
   const body = citation.replace(/\((.*)\)/, '$1');
-  const result = body.split(/[^A-Za-z0-9]+/).join('_');
-  console.warn("->", result);
-  return result;
+  return body.split(/[^A-Za-z0-9]+/).join('_');
 }
